92badf44df0987c72ed5c71e4fce3390c33582fe
Akshay Budhanavar <akshay.budhanavar2@gmail.com>
2026-02-16 11:44:17 +0530

    learn aboout manifest and defer

--- my_project/models/sample1/incremental_on_aws.sql
+++
@@ -1,9 +0,0 @@
-{{
-  config(
-    materialized = 'incremental',
-    unique_key = 'pokemon',
-    incremental_strategy = 'merge',
-    )
-}}
-
-select * from {{ source('learnsnow', 'load_csv_second_table') }} 
