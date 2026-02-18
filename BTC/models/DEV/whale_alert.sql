39260cde21ee29021bf0f4921bf3d1aa9587919e
Akshay Budhanavar <akshay.budhanavar2@gmail.com>
2026-02-18 23:39:09 +0530

    DBT SEED file created and fetched latest price

--- BTC/models/DEV/whale_alert.sql
+++ BTC/models/DEV/whale_alert.sql
@@ -1,4 +1,5 @@
-SELECT
+with whale_alert AS (
+  SELECT
     output_address,
     SUM(output_amount) AS total_output_amount,
     COUNT(output_address) AS address_count
@@ -7,4 +8,21 @@ FROM
 WHERE
     output_amount > 10
 GROUP BY
-    output_address
+    output_address ),
+
+latest_price AS (
+  SELECT
+    price
+  FROM
+    {{ ref('btc_usd_max') }} 
+    
+    WHERE  to_date ( replace(snapped_at,'UTC','')) = current_date() )
+
+select 
+    wa.output_address,
+    wa.total_output_amount,
+    round(wa.total_output_amount * lp.price, 2) AS total_output_value_usd,
+    wa.address_count
+from whale_alert wa
+cross join latest_price lp
+
